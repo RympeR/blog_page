67bf47e274dd6191ae8c0516fc8baaed8f532e9b
George <georg.rashkov@gmail.com>
2019-12-31 17:40:52 +0200

    thank you

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+news = [
+    {
+        'title': 'Наша первая запись',
+        'text': 'our ПРосто большой текст для 1й записи',
+        'date': '29.12.2019',
+        'author': 'Георгий'
+    },
+    {
+        'title': 'Наша первая запись',
+        'text': 'our ПРосто большой текст для 2й записи',
+        'date': '28.12.2019',
+
+    },
+]
+
+
 def home(request):
-    return HttpResponse('<h2>Привет мир</h2>')
-    
+    data = {
+        'news': news,
+        'title': 'Main blog page',
+    }
+    return render(request, 'blogapp/home.html', data)
+
+
 def contacts(request):
-    return HttpResponse('<h2>Наши контакты</h2>')
-
-
+    return render(request, 'blogapp/contacts.html', {'title': 'page about us'})
