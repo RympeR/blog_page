dabc6281a3fb37eab99da3218e3b59435bc5f4b3
George <georg.rashkov@gmail.com>
2020-01-03 14:45:42 +0200

    working

--- blog/settings.py
+++ blog/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'blogapp.apps.BlogappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
